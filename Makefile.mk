daa0f367cc7dfcd763d1d95c3b077fbefadbe84a
xldeng1230 <xldeng1203@498034487@qq.com>
2020-01-09 15:11:24 +0800

    makefile 目录结构提交

---
+++ Makefile.mk
@@ -0,0 +1,17 @@
+C_ARSG=
+# -D__INNER_DEBUG__PACKAGE_
+
+CPP_FLAG=-std=c++0x -Who-deprecated
+
+SHARED = 
+
+DEV_PATH=$(HOME)/wawa/Code/MyProject/Base 
+
+BASE_PATH=$(DEV_PATH)/base
+BASE_INC=-I$(BASE_PATH)/include
+BASE_LIB=-I$(BASE_PATH)/lib/base.a
+
+#CORE_PATH=$(DEV_PATH)/engine/core
+#CORE_INC=-I$(BASE_PATH)/include
+#CORE_LIB=-I$(BASE_PATH)/lib/base.a
+
---
+++ base/include/epoll_helper.h
@@ -0,0 +1,50 @@
+#ifndef __EPOLLHELPER_H__
+#define __EPOLLHELPER_H__
+
+#include <sys/epoll.h>
+#include <assert.h>
+
+typedef int (*funcEpollHandler) (int);
+
+class CEpollHelper
+{
+public:
+    void SetErrorHandler(funcEpollhandler pfError);
+    void SetReadHandler(funcEpollhandler pfRead);
+    void SetWriteHandler(funcEpollhandler pfWrite);
+public:
+    int EpollCreate(int iFDSize);
+    int EpollWait();
+    int EpollAdd(int iFd);
+    int EpollDelete(int iFd);
+    int EpollFocusOut(int iFd, bool bFocus = true);
+    int SetNonBlock(int iFd);
+
+    inline void SetEpollWaitingTime(int iEpollWaitingTime);
+
+    int GetEventErr()
+    {
+        return m_iEpollEventError;
+    }
+
+private:
+    funcEpollHandler m_pfError;
+    funcEpollHandler m_pfRead;
+    funcEpollHandler m_pfWrite;
+
+private:
+    int m_iEpollFd;
+    int m_iEpollEventSize;
+    int m_iEpollWaitingTime;
+    epoll_event* m_pstEpollEvent;
+    epoll_event m_stOneEpollEvent;
+    int m_iEpollEventError;
+
+};
+
+inline void CEpollHelper::SetEpollWaitingTime(int iEpollWaitingTime)
+{
+    m_iEpollWaitingTime = iEpollWaitingTime;
+}
+
+#endif
